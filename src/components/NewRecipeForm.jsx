8c2e26375326d198ca6b43c78f14f8844d92d6a7
Kirsi Jokela <kirsijo@gmail.com>
2022-05-16 11:54:58 +0300

    ingredients work

--- src/components/NewRecipeForm.jsx
+++ src/components/NewRecipeForm.jsx
@@ -3,17 +3,7 @@ import React from "react";
 import Nav from "./Nav";
 import { useEffect, useState } from "react";
 
-function NewRecipeForm({
-  submit,
-  name,
-  author,
-  country,
-  description,
-  ingredient,
-  quantity,
-  image,
-  instructions,
-}) {
+const NewRecipeForm = () => {
   const [countryData, setCountryData] = useState([]);
   const [formData, setFormData] = useState({
     name: "",
@@ -21,20 +11,16 @@ function NewRecipeForm({
     country: "",
     description: "",
     image: "",
-    ingredients: [
-      {
-        ingredient: "",
-        quantity: "",
-      },
-    ],
+    ingredients: [],
     instructions: "",
   });
 
-  useEffect(() => {
-    axios.get(`https://restcountries.com/v3.1/all`).then((response) => {
-      setCountryData(response.data);
-    });
-  }, []);
+  // ingredients have their own state
+  const [ingredients, setIngredients] = useState([
+    { id: 1, ingredient: "", quantity: "" },
+  ]);
+
+  /*FORM UPDATE AND SUBMIT HANDLERS*/
 
   const formUpdateHandler = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -48,19 +34,42 @@ function NewRecipeForm({
       .then((res) => console.log("res", res));
   };
 
+  /* get country data and country change handler*/
+
+  useEffect(() => {
+    axios.get(`https://restcountries.com/v3.1/all`).then((response) => {
+      setCountryData(response.data);
+    });
+  }, []);
+
+  const changeCountry = (e) => {
+    const correctCountry = countryData.find((c) => c.name === e.target.value);
+    setCountryData({ ...countryData, country: correctCountry.common });
+  };
+
+  /*INGREDIENT DATA */
+
+  const changeIngrData = (e, i) => {
+    const { name, value } = e.target;
+    const ingrList = [...ingredients];
+    ingrList[i][name] = value;
+    setIngredients(ingrList);
+    setFormData({ ...formData, ingredients: ingredients });
+  };
+
   return (
     <>
       <Nav />
       <div className="recipe-form">
         <h1>Add new recipe</h1>
-        <form onSubmit={submitHandler} onChange={formUpdateHandler}>
+        <form onSubmit={submitHandler}>
           <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
             id="name"
             required
-            defaultValue={name}
+            onChange={formUpdateHandler}
           />
           <label htmlFor="author">Author</label>
           <input
@@ -68,10 +77,15 @@ function NewRecipeForm({
             name="author"
             id="author"
             required
-            defaultValue={author}
+            onChange={formUpdateHandler}
           />
           <label htmlFor="country">Recipe is from</label>
-          <select type="text" name="country" id="country">
+          <select
+            type="text"
+            name="country"
+            id="country"
+            onChange={changeCountry}
+          >
             {countryData.map((country) => (
               <option value="country">{country.name.common}</option>
             ))}
@@ -81,32 +95,47 @@ function NewRecipeForm({
             type="textarea"
             name="description"
             id="description"
-            defaultValue={description}
+            onChange={formUpdateHandler}
           />
           <label htmlFor="image">Image</label>
-          <input type="text" name="image" id="image" defaultValue={image} />
+          <input
+            type="url"
+            name="image"
+            id="image"
+            onChange={formUpdateHandler}
+          />
           <h2>Ingredients</h2>
-          <div className="ingredients">
-            <label htmlFor="quantity">Ingredient</label>
-            <input
-              type="text"
-              name="ingredient"
-              id="ingredient"
-              defaultValue={ingredient}
-            />
-            <label htmlFor="quantity">Quantity</label>
-            <input
-              type="text"
-              name="quantity"
-              id="quantity"
-              defaultValue={quantity}
-            />
+          {ingredients.map((_, i) => {
+            return (
+              <div key={i}>
+                <div>
+                  <label htmlFor="ingredient">Ingredient</label>
+                  <input
+                    type="text"
+                    name="ingredient"
+                    id="ingredient"
+                    onChange={(e) => changeIngrData(e, i)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="quantity">Quantity</label>
+                  <input
+                    type="text"
+                    name="quantity"
+                    id="quantity"
+                    onChange={(e) => changeIngrData(e, i)}
+                  />
+                </div>
+              </div>
+            );
+          })}
+          <div>
             <label htmlFor="Instructions">Instructions</label>
             <input
               type="text"
               name="instructions"
               id="instructions"
-              defaultValue={instructions}
+              onChange={formUpdateHandler}
             />
             <input type="submit" value="send" />
           </div>
@@ -114,6 +143,6 @@ function NewRecipeForm({
       </div>
     </>
   );
-}
+};
 
 export default NewRecipeForm;
